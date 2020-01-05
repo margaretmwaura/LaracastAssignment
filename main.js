9846351c71d783e17b5599a85b1d40e8bf10ba97
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-05 21:11:59 +0300

    dispatching events

--- main.js
+++ main.js
@@ -1,8 +1,68 @@
+// ;
+Vue.component('task-list' , {
+
+    template: '<div><task v-for="task in tasks">{{task.description}}</task></div>',
+
+    data()
+    {
+        return{
+            tasks :
+                [
+                    {description:'Go to the store' , completed:false},
+                    {description: 'Go to the gym' , completed: false},
+                    {description:'Finish assignment' , completed:true},
+                    {description:'Go to church' , completed:true}
+                ]
+        }
+    }
+
+});
+
+
+
+
 Vue.component('task',{
     template : '<li><slot></slot></li>'
 });
 
+
+Vue.component('message',{
+
+    props : ['title','body'],
+    template : '<article class="message">\n' +
+        '  <div class="message-header">\n' +
+        '    <p>{{title}}</p>\n' +
+        '    <button class="delete" aria-label="delete"></button>\n' +
+        '  </div>\n' +
+        '  <div class="message-body">\n' +
+         '  <p>{{body}}</p>'+
+        '  </div>\n' +
+        '</article>'
+});
+
+Vue.component('coupon',{
+
+    template:'<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
+    methods : {
+        onCouponApplied()
+        {
+           this.$emit('applied')
+        }
+    }
+
+});
+
 new Vue({
 
-    el : '#root'
-})
+    el : '#roottwo',
+
+    data() {
+        return{
+
+        }
+
+    },
+    methods : {
+
+    }
+});
